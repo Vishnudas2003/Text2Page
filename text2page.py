0bb7c1e735f536bc24b606f963d5cc2cf65da1a0
vishnudas <vishnudas7412@gmail.com>
2023-09-18 14:15:33 -0400

    added gitgnore and updated the code

--- text2page.py
+++ text2page.py
@@ -5,57 +5,112 @@ import shutil
 # Define the version number
 VERSION = "1.0.0"  # Replace with your actual version number
 
-def create_html_from_txt(input_file, output_dir):
+def create_html_from_txt(input_file, output_dir, stylesheet_url=None):
     # Read the content of the input .txt file
     with open(input_file, 'r', encoding='utf-8') as txt_file:
         txt_content = txt_file.read()
     paragraphs = txt_content.split('\n\n')
     filename = os.path.basename(input_file).replace('.txt', '')
+
+    title_lines = txt_content.strip().split('\n')
+    if len(title_lines) >= 3 and not title_lines[0] and not title_lines[1] and not title_lines[2]:
+        title = title_lines[0]
+    else:
+        title = filename
+
     html_content = f'''<!doctype html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>{filename}</title>
+  <title>{title}</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-'''
+  '''
+  
+    if stylesheet_url:
+        html_content += f'<link rel="stylesheet" href="{stylesheet_url}">\n'
+    
+    html_content += '</head>\n<body>\n<h1>{title}</h1>\n'
+    
     for paragraph in paragraphs:
         html_content += f'  <p>{paragraph}</p>\n'
-        html_content += '</body>\n</html>\n'
+    
+    html_content += '</body>\n</html>\n'
+    
     os.makedirs(output_dir, exist_ok=True)
-
+    
     # Determine the output HTML file path
     output_file = os.path.join(output_dir, f'{filename}.html')
-
+    
     # Write the HTML content to the output file
     with open(output_file, 'w', encoding='utf-8') as html_file:
         html_file.write(html_content)
+    
+    print(f'Converted {input_file} to {output_file}')
+    # Read the content of the input .txt file
+    with open(input_file, 'r', encoding='utf-8') as txt_file:
+        txt_content = txt_file.read()
+    paragraphs = txt_content.split('\n\n')
+    filename = os.path.basename(input_file).replace('.txt', '')
+    
+    title_lines = txt_content.strip().split('\n')
+    if len(title_lines) >= 3 and not title_lines[0] and not title_lines[1] and not title_lines[2]:
+        title = title_lines[0]
+    else:
+        title = filename  
 
+    html_content = f'''<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>{title}</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="style.css">
+
+  '''
+    if stylesheet_url:
+        html_content += f'<link rel="stylesheet" href="{stylesheet_url}">\n'
+    
+    html_content += '</head>\n<body>\n<h1>{title}</h1>\n'
+    
+    for paragraph in paragraphs:
+        html_content += f'  <p>{paragraph}</p>\n'
+    
+    html_content += '</body>\n</html>\n'
+    
+    os.makedirs(output_dir, exist_ok=True)
+    
+    # Determine the output HTML file path
+    output_file = os.path.join(output_dir, f'{filename}.html')
+    
+    # Write the HTML content to the output file
+    with open(output_file, 'w', encoding='utf-8') as html_file:
+        html_file.write(html_content)
+    
     print(f'Converted {input_file} to {output_file}')
 
-def process_input(input_path, output_dir):
+def process_input(input_path, output_dir, stylesheet_url=None):
     if os.path.isfile(input_path) and input_path.endswith('.txt'):
-        create_html_from_txt(input_path, output_dir)
+        create_html_from_txt(input_path, output_dir, stylesheet_url)
     elif os.path.isdir(input_path):
-        process_directory(input_path, output_dir)
+        process_directory(input_path, output_dir, stylesheet_url)
     else:
         print(f"Error: {input_path} is not a valid .txt file or directory.")
 
-def process_directory(input_dir, output_dir):
+def process_directory(input_dir, output_dir, stylesheet_url=None):
     for root, dirs, files in os.walk(input_dir):
         for file in files:
             if file.endswith('.txt'):
-                create_html_from_txt(os.path.join(root, file), output_dir)
+                create_html_from_txt(os.path.join(root, file), output_dir, stylesheet_url)
 
 def main():
     parser = argparse.ArgumentParser(description='Process .txt files to .html with Text2page')
     parser.add_argument('path', nargs='?', help='path to the file or folder to be processed')
     parser.add_argument('--version', '-v', action='store_true', help='print the tool\'s name and version')
     parser.add_argument('--output', '-o', default='./text2page', help='Specify a different output directory (default: ./text2page)')
+    parser.add_argument('--stylesheet', '-s', help='URL to a CSS stylesheet for styling the HTML')
 
     args = parser.parse_args()
-
+    
     if args.version:
         print("Text2page Tool Version", VERSION)
         return
@@ -72,7 +127,7 @@ def main():
     if os.path.exists(args.output):
         shutil.rmtree(args.output)
 
-    process_input(args.path, args.output)
+    process_input(args.path, args.output, args.stylesheet)
 
 if __name__ == "__main__":
     main()
