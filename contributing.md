f57ec9935c2d11a442ffa8f1f05e65834a24bda0
Vishnu Das Puthukudi <94338168+Vishnudas2003@users.noreply.github.com>
2023-11-03 20:30:15 -0400

    Static analysis setup (#7)
    * Created and updated contributing.md
    
    * Add a source code formatter
    
    * Reverted commits
    
    * Add Black formatter and reformat code
    
    * Add linting
    
    * Updated the contributing.md for adding format(Black) and static Analysis(Flake)
    
    * Add Black formatter and Flake8 linting

---
+++ contributing.md
@@ -0,0 +1,86 @@
+# Contributing to Text2page
+
+Thank you for your interest in contributing to Text2page. We welcome contributions from the community to help improve our project. Please take a moment to read through this document to understand how you can contribute effectively.
+
+## Table of Contents
+- [Setting up the Development Environment](#setting-up-the-development-environment)
+- [How to Contribute](#how-to-contribute)
+- [Reporting Issues](#reporting-issues)
+- [Submitting Pull Requests](#submitting-pull-requests)
+- [Improving Documentation](#improving-documentation)
+- [Providing Feedback](#providing-feedback)
+- [Code of Conduct](#code-of-conduct)
+
+## Setting up the Development Environment
+
+To get started with development on Text2page, follow these steps:
+
+1. **Clone the Repository:**
+
+   ```bash
+   git clone https://github.com/Vishnudas2003/Text2Page.git
+
+2. Create a folder named textFiles in the project directory and place your .txt files inside it. This is where your input files will be located.
+
+## How to Contribute
+
+Contributions to Text2page can take several forms, including but not limited to:
+
+### Reporting Issues
+
+If you encounter any bugs or other problems with Text2page, please [open an issue](https://github.com/Vishnudas2003/Text2Page/issues)
+We welcome contributions from the community to help improve Text2page. If you are interested in contributing to Text2page, please follow these steps:
+
+### Submitting Pull Requests
+
+If you'd like to contribute code to the project, please follow these steps:
+
+1. Fork the repository on GitHub.
+2. Create a new branch from the master branch.
+3. Make your changes.
+4. Commit your changes and push your branch to GitHub.
+5. Open a pull request from your branch to the master branch of the Text2page repository.
+
+### Adding Code Formatting (Black) and Static Analysis (Flake)
+
+We've recently introduced two essential tools to enhance the code quality and maintain consistency within the Text2page project.
+
+**Black**: We have integrated Black, a powerful code formatter for Python, to ensure that our codebase is consistently well-formatted. Black helps maintain code readability and adherence to Python style guidelines. Setting up Black is straightforward. Ensure you have it installed:
+   
+   ```bash
+   pip install black
+   ```
+Before submitting any code changes, it's important to run Black on your Python files. You can do this by executing the following command in your project directory:
+
+   ```bash
+   black .
+   ```
+This will automatically format your code according to Black's style guidelines.
+
+**Flake8**: For static code analysis and style checking, we've incorporated Flake8. Flake8 is a helpful tool that identifies potential issues in your code, including style violations and common programming mistakes. It enforces adherence to PEP 8 guidelines and can significantly improve code quality. To set up Flake8, install it using:
+
+   ```bash
+   pip install flake8
+   ```
+Before submitting your code changes, it's essential to run Flake8 to identify and address any style or quality issues in your Python code. You can run Flake8 with the following command:
+
+   ```bash
+   flake8 .
+   ```
+Address any issues reported by Flake8 to maintain high code quality standards.
+
+By integrating Black for code formatting and Flake8 for static analysis, we aim to ensure that Text2page's codebase remains clean, consistent, and easy to maintain. These tools not only enhance the project's quality but also make it easier for contributors to collaborate effectively.
+
+
+### Improving Documentation
+
+We appreciate contributions to our documentation. If you find areas that need improvement, please submit a pull request with your proposed changes.
+
+### Providing Feedback
+
+If you have suggestions or feedback for the project, please feel free to contact us or discuss it in our community forums.
+
+#### Note: This CONTRIBUTING.md file is a work in progress, and we will be adding more details in future updates.
+```
+You can save this content in a file named `CONTRIBUTING.md` in the root of your project, as instructed. This file provides clear instructions for potential contributors on how to set up the development environment, report issues, submit pull requests, improve documentation, and follow the project's code of conduct. It's important to keep this document up to date as your project evolves.
+```
--- text2page.py
+++ text2page.py
@@ -3,11 +3,11 @@ import os
 import shutil
 import sys
 import markdown
-import re
 import tomllib
 
+
 def generate_html_content(title, content, stylesheet_url=None):
-    final_html_content = f'''<!doctype html>
+    final_html_content = f"""<!doctype html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
@@ -20,39 +20,43 @@ def generate_html_content(title, content, stylesheet_url=None):
 {content}
 </body>
 </html>
-'''
+"""
 
     return final_html_content
 
+
 def create_html_from_file(input_file, output_dir, stylesheet_url=None):
     _, extension = os.path.splitext(input_file)
-    with open(input_file, 'r', encoding='utf-8') as file:
+    with open(input_file, "r", encoding="utf-8") as file:
         input_content = file.read()
 
-    if extension.lower() == '.txt':
+    if extension.lower() == ".txt":
         parsed_content = input_content
-        output_filename = os.path.basename(input_file).replace('.txt', '')
-    elif extension.lower() == '.md':
+        output_filename = os.path.basename(input_file).replace(".txt", "")
+    elif extension.lower() == ".md":
         parsed_content = input_content
-        output_filename = os.path.basename(input_file).replace('.md', '')
+        output_filename = os.path.basename(input_file).replace(".md", "")
     else:
         print(f"Unsupported file type: {extension}")
         return
 
     title = output_filename
     # Convert the Markdown content
-    parsed_content = markdown.markdown(parsed_content, extensions=['markdown.extensions.fenced_code'])
+    parsed_content = markdown.markdown(
+        parsed_content, extensions=["markdown.extensions.fenced_code"]
+    )
 
     final_html_content = generate_html_content(title, parsed_content, stylesheet_url)
 
     os.makedirs(output_dir, exist_ok=True)
 
-    output_file = os.path.join(output_dir, f'{output_filename}.html')
+    output_file = os.path.join(output_dir, f"{output_filename}.html")
 
-    with open(output_file, 'w', encoding='utf-8') as html_file:
+    with open(output_file, "w", encoding="utf-8") as html_file:
         html_file.write(final_html_content)
 
-    print(f'Converted {input_file} to {output_file}')
+    print(f"Converted {input_file} to {output_file}")
+
 
 def process_input(input_path, output_dir, stylesheet_url=None):
     if os.path.isfile(input_path):
@@ -62,19 +66,41 @@ def process_input(input_path, output_dir, stylesheet_url=None):
     else:
         print(f"Error: {input_path} is not a valid .txt or .md file or directory.")
 
+
 def process_directory(input_dir, output_dir, stylesheet_url=None):
     for root, dirs, files in os.walk(input_dir):
         for file in files:
-            if file.endswith('.txt'):
-                create_html_from_file(os.path.join(root, file), output_dir, stylesheet_url)
+            if file.endswith(".txt"):
+                create_html_from_file(
+                    os.path.join(root, file), output_dir, stylesheet_url
+                )
+
 
 def main():
-    parser = argparse.ArgumentParser(description='Process .txt or .md files to .html with Text2page')
-    parser.add_argument('path', nargs='?', help='path to the file or folder to be processed')
-    parser.add_argument('--version', '-v', action='store_true', help="print the tool's name and version")
-    parser.add_argument('--output', '-o', default='./text2page', help='Specify a different output directory (default: ./text2page)')
-    parser.add_argument('--stylesheet', '-s', help='URL to a CSS stylesheet for styling the HTML')
-    parser.add_argument("--config", "-c", metavar="<config.toml>", help="If you want to use a TOML config file to set the arguments needed for the program")
+    parser = argparse.ArgumentParser(
+        description="Process .txt or .md files to .html with Text2page"
+    )
+    parser.add_argument(
+        "path", nargs="?", help="path to the file or folder to be processed"
+    )
+    parser.add_argument(
+        "--version", "-v", action="store_true", help="print the tool's name and version"
+    )
+    parser.add_argument(
+        "--output",
+        "-o",
+        default="./text2page",
+        help="Specify a different output directory (default: ./text2page)",
+    )
+    parser.add_argument(
+        "--stylesheet", "-s", help="URL to a CSS stylesheet for styling the HTML"
+    )
+    parser.add_argument(
+        "--config",
+        "-c",
+        metavar="<config.toml>",
+        help="Use a TOML config file for program arguments",
+    )
 
     args = parser.parse_args()
 
@@ -110,6 +136,7 @@ def main():
             process_input(args.path, output, stylesheet)
     else:
         process_input(args.path, args.output, args.stylesheet)
+
 
 if __name__ == "__main__":
     main()
